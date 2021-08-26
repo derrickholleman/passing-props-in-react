fcf189e2f2b57cd1ec5dd575184ebe6d42d9b033
Derrick Holleman <derrickholleman@gmail.com>
2021-08-26 04:06:44 -0500

    delete comment

--- src/App.js
+++ src/App.js
@@ -4,23 +4,13 @@ import AddTodo from './Components/AddTodo';
 
 export default function App() {
 
-  // set initial value of state and is passed to {this.state.todos}
+  // set initial value of state 
   const [count, setCount] = useState(5)
 
   // initialize addOne method and setState function to increment state by +1 on click
   const addOne = () => {
     setCount((count) => count + 1)
   }
-
-  /*
-      add a method called addOne() that increments
-      this.state.todos by 1
-
-      pass the method down to the AddTodo component
-      tie it to the onClick event of the button inside of AddTodo
-
-      do not touch state or any of the other JSX
-    */
 
     return (
       <div className="App">
@@ -32,4 +22,3 @@ export default function App() {
       </div>
     );
 }
-// {this.addOne} in child component is passed from addOne method. addTodo is initialized as props and then used in an onClick function in child component.
